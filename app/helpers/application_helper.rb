328bb06d346c0c4290d08288fd3880a8a9bf3cef
erijawa <e.akgw0913@gmail.com>
2024-10-22 10:25:32 +0900

    [Add] #12 フラッシュメッセージ用の背景色を設定するヘルパーメソッドを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when :notice then "bg-info-500"
+    when :alert  then "bg-error-500"
+    when :error  then "bg-warning-500"
+    else "bg-gray-500"
+    end
+  end
 end
