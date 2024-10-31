b73fb8d49ebd27190105f987123c38e3bac3d92e
erijawa <e.akgw0913@gmail.com>
2024-10-31 15:28:11 +0900

    [WIP] #26 手順フォームの追加、削除機能を実装
    手順番号の振り直しはこれから行う

--- app/javascript/post_form.js
+++ app/javascript/post_form.js
@@ -16,10 +16,10 @@ addIngredientButton.addEventListener('click', function() {
     <div class="flex">
         <input name="post_form[ingredients_name][]" placeholder="材料名" class="input input-bordered input-info form-control mb-2 mr-1 w-full mx-auto basis-4/6" type="text" id="post_form_ingredients_name">
         <input name="post_form[ingredients_quantity][]" placeholder="分量" class="input input-bordered input-info form-control mb-2 w-full mx-auto basis-2/6" type="text" id="post_form_ingredients_quantity">
-        <button type="button" class="btn btn-ghost delete_ingredient_button">X</button>
+        <button type="button" class="btn btn-ghost delete_button">X</button>
     </div>
   `;
-  set_delete_btn_disabled();
+  set_delete_btn_disabled(ingredientFields);
   set_add_btn_disabled();
 });
 
@@ -30,13 +30,13 @@ ingredientFields.addEventListener('click', (event) => {
     element = element.parentNode;
     element.remove();
 }
-  set_delete_btn_disabled();
+  set_delete_btn_disabled(ingredientFields);
   set_add_btn_disabled();
 });
 
 // 削除ボタンの無効化(要素数が1つの場合のみ無効)
-function set_delete_btn_disabled() {
-  let buttons = ingredientFields.getElementsByClassName('delete_ingredient_button');
+function set_delete_btn_disabled(field) {
+  let buttons = field.getElementsByClassName('delete_button');
   if (buttons.length == 1) {
     buttons[0].disabled = true;
   }
@@ -47,13 +47,55 @@ function set_delete_btn_disabled() {
   }
 }
 
-// 追加ボタンの無効化
+// 追加ボタンの無効化(材料フィールド)
 function set_add_btn_disabled() {
-  let buttons = ingredientFields.getElementsByClassName('delete_ingredient_button');
+  let buttons = ingredientFields.getElementsByClassName('delete_button');
   if (buttons.length < 10) {
     addIngredientButton.disabled = false;
   }
   else {
     addIngredientButton.disabled = true;
+  }
+}
+
+// 手順フォーム
+const stepFields = document.getElementById('step_fields');
+
+// 手順フォームの追加
+const addStepButton = document.getElementById('add_step_button');
+addStepButton.addEventListener('click', function() {
+  let num = stepFields.childElementCount;
+  stepFields.innerHTML += `
+    <div class="flex items-center mb-2">
+        <div class="inline w-10 h-10 rounded-full bg-[#8DBA30] text-center leading-10">${num}</div>
+        <input name="post_form[steps_instruction][]" class="input input-bordered input-info form-control ml-1 w-full mx-auto inline" type="text" id="post_form_steps_instruction">
+        <button type="button" class="btn btn-ghost delete_button">X</button>
+    </div>
+  `;
+  set_delete_btn_disabled(stepFields);
+  set_add_step_btn_disabled();
+});
+
+// 手順フォームの削除
+stepFields.addEventListener('click', (event) => {
+  let element = event.target;
+  if (element instanceof HTMLButtonElement) {
+    element = element.parentNode;
+    element.remove();
+  }
+  
+
+  set_delete_btn_disabled(stepFields);
+  set_add_step_btn_disabled();
+});
+
+// 追加ボタンの無効化(手順フィールド)
+function set_add_step_btn_disabled() {
+  let buttons = stepFields.getElementsByClassName('delete_button');
+  if (buttons.length < 8) {
+    addStepButton.disabled = false;
+  }
+  else {
+    addStepButton.disabled = true;
   }
 }
