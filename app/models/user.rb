29e638ff2a633bce7e78a41849e096f08bc1206b
erijawa <e.akgw0913@gmail.com>
2024-11-05 14:18:01 +0900

    [Add] #28 詳細画面に編集リンクを設定、投稿者本人ログイン時のみ表示されるよう条件分岐

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :name, presence: true, length: { maximum: 255 }
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+
+  def own?(object)
+    id == object&.user_id
+  end
 end
