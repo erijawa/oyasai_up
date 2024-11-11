8b0863e38770786b91bc3e2e1f13d1cb1db1ec5c
erijawa <e.akgw0913@gmail.com>
2024-11-11 15:08:53 +0900

    [Add] #34 VegetableLog、Userのコントローラーのアクションを定義

--- app/controllers/vegetable_logs_controller.rb
+++ app/controllers/vegetable_logs_controller.rb
@@ -1,9 +1,23 @@
 class VegetableLogsController < ApplicationController
   def create
-
+    @vegetable_log = current_user.vegetable_logs.build(vegetable_log_params)
+    if @vegetable_log.calculate_total && @vegetable_log.save
+      redirect_to user_path(current_user), success: "記録しました"
+    else
+      flash.now[:danger] = "記録できませんでした"
+    end
   end
 
   def update
+    @vegetable_log = current_user.vegetable_logs.find(params[:id])
+    if @vegetable_log.update(vegetable_log_params) && @vegetable_log.calculate_total
+      redirect_to user_path(current_user), success: "記録しました"
+    else
+      flash.now[:danger] = "記録できませんでした"
+    end
+  end
 
+  def vegetable_log_params
+    params.require(:vegetable_log).permit(:breakfast, :lunch, :dinner).merge(date: Time.zone.today)
   end
 end
--- app/models/vegetable_log.rb
+++ app/models/vegetable_log.rb
@@ -2,14 +2,20 @@ class VegetableLog < ApplicationRecord
   with_options presence: true do
     validates :breakfast
     validates :lunch
-    validates :diner
+    validates :dinner
     validates :total
   end
   validates :date, presence: true, uniqueness: { scope: :user_id }
   belongs_to :user
 
-  enum :breakfast, { none: 0, had_vegetable: 1 }, validate: true
-  enum :lunch, { none: 0, had_vegetable: 1 }, validate: true
-  enum :diner, { none: 0, had_vegetable: 1 }, validate: true
+  enum :breakfast, { no_vegetable: 0, had_vegetable: 1 }, validate: true, prefix: true
+  enum :lunch, { no_vegetable: 0, had_vegetable: 1 }, validate: true, prefix: true
+  enum :dinner, { no_vegetable: 0, had_vegetable: 1 }, validate: true, prefix: true
+
+  def calculate_total
+    total = self.breakfast_before_type_cast + self.lunch_before_type_cast + self.dinner_before_type_cast
+    p self.breakfast_before_type_cast
+    self.update(total: total)
+  end
 
 end
