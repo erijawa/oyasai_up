02ecda798c96f03999792d74ba3b33a61861ec03
erijawa <e.akgw0913@gmail.com>
2024-11-12 10:22:40 +0900

    [WIP] #34 vegetable_logsコントローラーのcreate,updateアクションが機能するよう修正

--- app/controllers/vegetable_logs_controller.rb
+++ app/controllers/vegetable_logs_controller.rb
@@ -1,23 +1,32 @@
 class VegetableLogsController < ApplicationController
   def create
-    @vegetable_log = current_user.vegetable_logs.build(vegetable_log_params)
+    breakfast = vegetable_log_params[:breakfast].to_i
+    lunch = vegetable_log_params[:lunch].to_i
+    dinner = vegetable_log_params[:dinner].to_i
+    @vegetable_log = current_user.vegetable_logs.build(breakfast: breakfast, lunch: lunch, dinner: dinner, date: Time.zone.today)
+    p @vegetable_log
     if @vegetable_log.save && @vegetable_log.calculate_total
-      redirect_to user_path(current_user), success: "記録しました"
+      redirect_to user_path(current_user), notice: "記録しました"
     else
-      redirect_to user_path(current_user), success: "記録できませんでした", status: :unprocessable_entity
+      redirect_to user_path(current_user), notice: "記録できませんでした", status: :unprocessable_entity
     end
   end
 
   def update
     @vegetable_log = current_user.vegetable_logs.find(params[:id])
-    if @vegetable_log.update(vegetable_log_params) && @vegetable_log.calculate_total
-      redirect_to user_path(current_user), success: "記録しました"
+    breakfast = vegetable_log_params[:breakfast].to_i
+    lunch = vegetable_log_params[:lunch].to_i
+    dinner = vegetable_log_params[:dinner].to_i
+    if @vegetable_log.update(breakfast: breakfast, lunch: lunch, dinner: dinner) && @vegetable_log.calculate_total
+      redirect_to user_path(current_user), notice: "記録しました"
     else
-      flash.now[:danger] = "記録できませんでした"
+      redirect_to user_path(current_user), notice: "記録できませんでした", status: :unprocessable_entity
     end
   end
 
+  private
+
   def vegetable_log_params
-    params.require(:vegetable_log).permit(:breakfast, :lunch, :dinner).merge(date: Time.zone.today)
+    params.require(:vegetable_log).permit(:breakfast, :lunch, :dinner)
   end
 end
