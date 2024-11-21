057a8a47efa84c6d4c372d5f40d247854cca3a91
erijawa <e.akgw0913@gmail.com>
2024-11-21 14:02:34 +0900

    [Add] #32 Userモデルにavatarカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_11_045625) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_21_045229) do
   create_table "post_tags", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "post_id", null: false
     t.bigint "tag_id", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_11_045625) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
