8c786cf0935ff5ac379a32cd10ca02d9bda02c75
erijawa <e.akgw0913@gmail.com>
2024-11-21 10:23:20 +0900

    [Add] #14 複数のlocaleファイルが読み込めるよpathを記述

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module Myapp
     config.active_record.default_timezone = :local
     # デフォルトのlocaleを日本語(:ja)にする
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
