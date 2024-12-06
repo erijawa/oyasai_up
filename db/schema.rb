f2582ec330cfebb2416d5f30cb3479cac65d81af
erijawa <e.akgw0913@gmail.com>
2024-12-06 12:50:54 +0900

    [Add] #48 UserモデルにLINEログイン用のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_06_010633) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_06_034942) do
   create_table "bookmarks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
@@ -86,6 +86,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_06_010633) do
     t.datetime "updated_at", null: false
     t.string "avatar"
     t.integer "level", default: 0, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
