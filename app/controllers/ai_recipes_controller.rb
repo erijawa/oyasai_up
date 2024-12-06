56f5e5a9c9096dc40986141162161231b1a00eca
erijawa <e.akgw0913@gmail.com>
2024-12-06 14:55:44 +0900

    [Fix] AIのプロンプトを修正
    既存の手順にとらわれないよう、プロンプトから元のレシピの手順を削除し、材料のみを指定するよう変更

--- app/controllers/ai_recipes_controller.rb
+++ app/controllers/ai_recipes_controller.rb
@@ -12,8 +12,7 @@ class AiRecipesController < ApplicationController
     end
     @post = Post.find(params[:post_id])
     former_ingredients = @post.recipe_ingredients.to_json(only: [ :name, :quantity ])
-    former_steps = @post.recipe_steps.to_json(only: [ :order, :instruction ])
-    @response = JSON.parse(Openai::ApiResponseService.new.call(former_ingredient_name, new_ingredient_name, former_ingredients, former_steps))
+    @response = JSON.parse(Openai::ApiResponseService.new.call(former_ingredient_name, new_ingredient_name, former_ingredients))
     @post_form = PostForm.new
     if @response.include?("999")
       flash.now[:alert] = "食材を入力してください"
