bfa22a563d8149cb7e4c6a73605d5ccb9c0da455
erijawa <e.akgw0913@gmail.com>
2024-12-06 10:07:31 +0900

    [Add] #47 Userモデルにレベル機能追加用のlevelカラムを設置

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_27_012834) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_06_010633) do
   create_table "bookmarks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
@@ -85,6 +85,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_27_012834) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.integer "level", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
