ed71f8cb52f0bc52ba99a74a0c63de4eb4cb9d06
erijawa <e.akgw0913@gmail.com>
2024-12-13 13:01:34 +0900

    [Fix] ローディングアニメーションを修正

--- app/javascript/loading.js
+++ app/javascript/loading.js
@@ -1,4 +1,8 @@
 document.addEventListener('turbo:load', ()=>{
-  const spinner = document.getElementById("loading");
+  let spinner = document.getElementById("loading");
+  spinner.classList.add("loaded");
+})
+document.addEventListener('turbo:render', ()=>{
+  let spinner = document.getElementById("loading");
   spinner.classList.add("loaded");
 })
