8d446338e01ac217c72b2c51de3825913458919c
erijawa <e.akgw0913@gmail.com>
2024-12-13 10:28:44 +0900

    [Fix] ローディングアニメーションがフォームのrender時に消えないバグを修正

--- app/javascript/loading.js
+++ app/javascript/loading.js
@@ -2,3 +2,7 @@ document.addEventListener('turbo:load', ()=>{
   const spinner = document.getElementById("loading");
   spinner.classList.add("loaded");
 })
+document.addEventListener('turbo:render', ()=>{
+  const spinner = document.getElementById("loading");
+  spinner.classList.add("loaded");
+})
