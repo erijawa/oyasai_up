6893b3a71418ef0008e569c88872f26ece039077
erijawa <e.akgw0913@gmail.com>
2024-12-23 11:44:44 +0900

    [Test] #57 RSpecの設定、Userモデルのテストファイル作成

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  it "名前、メールアドレス、パスワードがあれば登録できる" do
+    user = FactoryBot.build(:user)
+    expect(user).to be_valid
+  end
+  it "名前がなければ登録できない" do
+    expect(FactoryBot.build(:user, name: "")).to_not be_valid
+  end
+  it "メールアドレスがなければ登録できない" do
+    expect(FactoryBot.build(:user, email: "")).to_not be_valid
+  end
+  it "重複したメールアドレスの場合、無効である" do
+    user1 = FactoryBot.create(:user,name: "taro", email: "taro@example.com")
+    expect(FactoryBot.build(:user, name: "jiro", email: user1.email)).to_not be_valid
+  end
+  it "パスワードがなければ登録できない" do
+    expect(FactoryBot.build(:user, password: "")).to_not be_valid
+  end
+  it "password_confirmationとpasswordが異なる場合保存できない" do
+    expect(FactoryBot.build(:user,password:"password",password_confirmation: "passward")).to_not be_valid
+  end
+end
