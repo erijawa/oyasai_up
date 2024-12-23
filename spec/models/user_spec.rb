7359a757cdf532d100d78997cb016346951634af
erijawa <e.akgw0913@gmail.com>
2024-12-23 14:36:32 +0900

    [Test] #57 Userモデルのテストを修正、追記

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,35 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it "名前、メールアドレス、パスワードがあれば登録できる" do
-    user = FactoryBot.build(:user)
+  it "名前、メールアドレスがあり、パスワードが6文字以上であれば有効であること" do
+    user = build(:user)
     expect(user).to be_valid
   end
-  it "名前がなければ登録できない" do
-    expect(FactoryBot.build(:user, name: "")).to_not be_valid
+
+  it '名前は30文字以下であること' do
+    user = build(:user)
+    user.name = 'a' * 31
+    user.valid?
+    expect(user.errors[:name]).to include('は30文字以内で入力してください')
   end
-  it "メールアドレスがなければ登録できない" do
-    expect(FactoryBot.build(:user, email: "")).to_not be_valid
+
+  it "名前、メールアドレス、パスワードは必須項目であること" do
+    user = build(:user)
+    user.email = nil
+    user.name = nil
+    user.password = nil
+    user.valid?
+    expect(user.errors[:email]).to include('を入力してください')
+    expect(user.errors[:name]).to include('を入力してください')
+    expect(user.errors[:password]).to include('を入力してください')
+
   end
-  it "重複したメールアドレスの場合、無効である" do
-    user1 = FactoryBot.create(:user,name: "taro", email: "taro@example.com")
-    expect(FactoryBot.build(:user, name: "jiro", email: user1.email)).to_not be_valid
-  end
-  it "パスワードがなければ登録できない" do
-    expect(FactoryBot.build(:user, password: "")).to_not be_valid
-  end
-  it "password_confirmationとpasswordが異なる場合保存できない" do
-    expect(FactoryBot.build(:user,password:"password",password_confirmation: "passward")).to_not be_valid
+
+  it "重複したメールアドレスの場合、無効であること" do
+    user1 = create(:user)
+    user2 = build(:user)
+    user2.email = user1.email
+    user2.valid?
+    expect(user2.errors[:email]).to include("はすでに存在します")
   end
 end
